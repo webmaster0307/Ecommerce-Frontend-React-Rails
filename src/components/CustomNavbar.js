d7bae75fd0da3354c514088b4cee6067dc3b46f8
webmaster0307 <web.master0307@gmail.com>
2019-10-11 12:43:01 -0400

    Change from class to functional component

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -2,32 +2,30 @@ import React, { Component } from 'react';
 import { Link, NavLink} from 'react-router-dom'
 import {Navbar, Nav, NavItem, Form, FormControl, Button, NavDropdown } from 'react-bootstrap'
 
-class CustomNavbar extends Component {
-    state = {  }
-    render() { 
+
+const CustomNavbar = () =>  {
         return ( 
           <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-          <Navbar.Brand href="/">Company Logo</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Company Logo</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#categories">Categories</Nav.Link>
-              <NavDropdown title="My Account" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#Log In">Log In</NavDropdown.Item>
-                <NavDropdown.Item href="#Sign Up">Sign Up</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#">Wish List</Nav.Link>
-              <Nav.Link eventKey={2} href="#">Cart</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link as={Link} to="/About">About</Nav.Link>
+                <Nav.Link as={Link} to="/Categories">Categories</Nav.Link>
+                <NavDropdown title="My Account" id="collasible-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="/Login">Log In</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/Signup">Sign Up</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to="/Logout">Logout</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link as={Link} to="/Wishlist">Wish List</Nav.Link>
+                <Nav.Link eventKey={2} as={Link} to="/Cart">Cart</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
         </Navbar>
          );
-    }
 }
  
 export default CustomNavbar;
