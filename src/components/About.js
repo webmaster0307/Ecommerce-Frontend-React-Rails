63deb30b1150264299ef33e826a6b3d69c432d0b
webmaster0307 <web.master0307@gmail.com>
2019-10-11 13:04:38 -0400

    Add dummy text

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>About Page</div>
+        <div className = "container">
+            <h1>About Page</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis fermentum tristique. Cras lobortis arcu vel vehicula sodales. Phasellus scelerisque massa a ligula dapibus sodales. Curabitur faucibus euismod orci. Curabitur non hendrerit lectus, at sollicitudin erat. Donec sed est a neque facilisis finibus ut at nibh. Praesent faucibus vestibulum sapien, sed tincidunt lacus suscipit eu.</h2>
+            <h2>Integer iaculis mi cursus justo commodo maximus. Suspendisse eleifend nunc sem, quis congue libero venenatis quis. Morbi vitae justo enim. Donec quis augue placerat, consectetur urna eget, mollis nulla. Vivamus porta enim in orci egestas scelerisque. Proin ac blandit eros, non molestie nisi. Ut velit nisl, porttitor nec urna euismod, pellentesque volutpat quam.</h2>
+        </div>
     )
 }
 
