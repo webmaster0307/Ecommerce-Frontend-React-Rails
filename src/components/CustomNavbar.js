4c0a6a4861c3c4c22ed85f42e8457fd6044cb784
webmaster0307 <web.master0307@gmail.com>
2019-10-11 11:41:00 -0400

    Update navbar names

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -2,31 +2,27 @@ import React, { Component } from 'react';
 import { Link, NavLink} from 'react-router-dom'
 import {Navbar, Nav, NavItem, Form, FormControl, Button, NavDropdown } from 'react-bootstrap'
 
-
 class CustomNavbar extends Component {
     state = {  }
     render() { 
         return ( 
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+          <Navbar.Brand href="/">Company Logo</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#categories">Categories</Nav.Link>
+              <NavDropdown title="My Account" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#Log In">Log In</NavDropdown.Item>
+                <NavDropdown.Item href="#Sign Up">Sign Up</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
+              <Nav.Link href="#">Wish List</Nav.Link>
+              <Nav.Link eventKey={2} href="#">Cart</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
