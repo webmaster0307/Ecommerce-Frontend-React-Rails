78ad68315578cacea1595becc22ec3f2ad490131
webmaster0307 <web.master0307@gmail.com>
2019-10-12 09:47:41 -0400

    Work on nav bar for mobile devices. Add font awesome bar icon

--- src/components/NewNavbar.js
+++ src/components/NewNavbar.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink} from 'react-router-dom'
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const NewNavbar = () =>  { 
-    return (
-        <nav class="navbar">
+
+class NewNavBar extends Component {
+    state = {  }
+
+
+    handleOnClick = (e) => {
+        let mainNav= document.getElementById('js-menu')
+        let NavBarToggle = document.getElementById('js-navbar-toggle')
+        mainNav.classList.toggle('active')
+    }
+
+    render() { 
+        return ( 
+            <nav class="navbar">
             <span class="navbar-toggle" id="js-navbar-toggle">
-                 <FontAwesomeIcon icon={faBars} />
+                 <FontAwesomeIcon icon={faBars} onClick={this.handleOnClick} />
             </span>
             <Link to="/" class="logo">Company Logo</Link>
-            <ul class="main-nav" id="js-main">
+            <ul class="main-nav" id="js-menu">
                 <li><Link to="/About" class="nav-links">About</Link></li>
                 <li><Link to="/Categories" class="nav-links">Categories</Link></li>
                 <li>
@@ -18,12 +29,14 @@ const NewNavbar = () =>  {
                     <ul class="nav-dropdown">
                         <li><Link to="/Signup" class="nav-links">Signup</Link></li>
                         <li><Link to="/Login" class="nav-links">Login</Link></li>
-                        <li><Link to="/Logout" class="nav-links">Login</Link></li>
+                        <li><Link to="/Logout" class="nav-links">Logout</Link></li>
                     </ul>
                 </li>
             </ul>
         </nav>
-    )
+         );
+    }
 }
+ 
+export default NewNavBar;
 
-export default NewNavbar
