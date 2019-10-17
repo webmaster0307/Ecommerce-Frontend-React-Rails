5f43200f31618fa6f0009594be04e7e16881ce90
webmaster0307 <web.master0307@gmail.com>
2019-10-16 21:55:43 -0400

    Update notes

---
+++ src/components/auth/Logout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Logout extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                Logout Form
+            </div>
+          );
+    }
+}
+ 
+export default Logout;
