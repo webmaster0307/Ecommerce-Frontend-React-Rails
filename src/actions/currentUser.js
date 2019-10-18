95dfc22fa28293b471a15b75c95631c92018d3a5
webmaster0307 <web.master0307@gmail.com>
2019-10-17 23:08:27 -0400

    Add login and get_current_user functions

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -6,3 +6,51 @@ export const setCurrentUser = user => {
       user
     }
   }
+
+  // asynchronous action creators
+    export const login = (credentials) => {
+
+      console.log(credentials);
+      return dispatch => {
+        return fetch("http://localhost:3001/api/v1/login", {
+          credentials: "include",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(credentials)
+        })
+          .then(r => r.json())
+          .then(response => {
+            // console.log(response);
+            if (response.error) {
+              alert(response.error)
+            } else {
+              dispatch(setCurrentUser(response))
+              
+            }
+          })
+          .catch(console.log)
+      }
+    }
+
+    export const getCurrentUser = () => {
+      return dispatch => {
+        return fetch("http://localhost:3001/api/v1/get_current_user", {
+          credentials: "include",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          },
+        })
+          .then(r => r.json())
+          .then(response => {
+            if (response.error) {
+              alert(response.error)
+            } else {
+              dispatch(setCurrentUser(response))
+            }
+          })
+          .catch(console.log)
+      }
+    }
