f6cb9d14f7822df08e84712799c6dbdbaf845156
webmaster0307 <web.master0307@gmail.com>
2019-10-17 23:07:57 -0400

    Add mapStateToProps for login and current user

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
 import Home from './containers/Home'
-import Login from './components/auth/Login'
+import Login from './components/Login'
 import Register from './components/auth/Register'
-import Logout from './components/auth/Logout'
+import Logout from './components/Logout'
 import About from './components/About'
 import Wishlist from './components/Wishlist'
 import Cart from './components/Cart'
 import NewNavbar from './components/NewNavbar'
 import NewBusiness from './components/NewBusiness'
 import Businesses from './components/Businesses'
+import { getCurrentUser } from "./actions/currentUser.js"
 
 
 class App extends Component {
 
   
-
-
   componentDidMount() {
-    fetch('http://localhost:3001/api/v1/users')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    this.props.getCurrentUser()
   }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3001/api/v1/users')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
   render() { 
     return ( 
@@ -30,10 +33,10 @@ class App extends Component {
           <div className="App">
             <NewNavbar />
             <Switch>
+              <Route path='/logout' component={Logout} />
               <Route exact path='/' component={Home} />
               <Route path='/login' component={Login} />
               <Route path='/signup' component={Register} />
-              <Route path='/logout' component={Logout} />
               <Route path='/about' component={About} />
               <Route path='/wishlist' component={Wishlist} />
               <Route path='/cart' component={Cart} />
@@ -46,6 +49,12 @@ class App extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser,
+  })
+}
  
-export default (connect(null)(App));
+export default (connect(mapStateToProps, {getCurrentUser})(App));
 
