a3f7638c7832d6fe573ad3196e9a4db829b1804f
webmaster0307 <web.master0307@gmail.com>
2019-10-20 23:01:50 -0400

    Delete logout component.  Move to nav bar link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/auth/Register'
-import Logout from './components/Logout'
 import About from './components/About'
 import Wishlist from './components/Wishlist'
 import Cart from './components/Cart'
@@ -36,7 +35,6 @@ class App extends Component {
             <Navbar />
             <Search />
             <Switch>           
-              {/* <Route path='/logout' component={Logout} /> */}
               <Route exact path='/' component={Home} />
               <Route path='/login' component={Login} />
               <Route path='/signup' component={Register} />
