578e032b2b0e1aa3c6c3c478e3133187cd047f3f
webmaster0307 <web.master0307@gmail.com>
2019-10-25 09:35:51 -0400

    Update format

--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -1,16 +1,4 @@
-// const initialState = []
-
-// export default (state = initialState, action) => {
-//     switch (action.type) {
-//       case "FETCH_CATEGORIES":
-//         return action.categories
-//         default:
-//             return state
-//     }
-//  }
-
-
- export default (state = {categories: []}, action) => {
+export default (state = {categories: []}, action) => {
   switch (action.type) {
     case "FETCH_CATEGORIES":
       return {categories: action.categories}
