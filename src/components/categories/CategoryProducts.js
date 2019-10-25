472054dd2b626d0ba48393e50aae610c0190c141
webmaster0307 <web.master0307@gmail.com>
2019-10-25 13:32:24 -0400

    Add link to product

--- src/components/categories/CategoryProducts.js
+++ src/components/categories/CategoryProducts.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 
 const CategoryProducts = (props) => {
-
 
     console.log("categoryProducts", props)
     // console.log(props.products.match.params.id[0])
     // console.log(props.products.categories.categories.products)
 
     let productList = props.products.categories.categories.filter(category => category.id == props.products.match.params.id)[0]
-
+    const categoryId = props.products.match.params.id[0]
     console.log("products", productList);
 
     return (
         <div className="">
             {productList ? productList.attributes.products.map(product => 
             <div>
+                <Link to={`/category/${categoryId}/product/${product.id}` }>
                 <li> { product.name } </li>
+                </Link>
             </div>
             )
             : null}
