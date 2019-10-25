fcaf53d71304e6242c1b075fd389ebf982c9eb26
webmaster0307 <web.master0307@gmail.com>
2019-10-25 11:08:06 -0400

    Create products component

---
+++ src/components/products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Products = (props) => {
+
+    console.log("product props", props)
+
+    return(
+        <div>
+            Products by Category
+        </div>
+    )
+}
+
+export default Products;
