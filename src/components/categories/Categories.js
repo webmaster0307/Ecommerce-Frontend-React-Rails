431063855d6250b2b970ed6897f31b52e94a83c7
webmaster0307 <web.master0307@gmail.com>
2019-10-26 07:58:26 -0400

    Change image size

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom'
 
 const Categories = (props) => {
     
-    // console.log("categories props", props)
+    console.log("categories props", props)
     // console.log("categoriesList.categories is", props.categoriesList.categories)
        
     return (
         <div className="categories">
             {props.categoriesList.categories ? props.categoriesList.categories.map(category => 
             <div key={category.id}>
-             <Link to={`/category/${category.id}` }>
-                <li className="categories-image">image</li>
-                <li className="categories-image">{ category.attributes.name } </li>
+           
+            <Link to={`/category/${category.id}` }>
+                {/* <li className="categories-image">{ category.attributes.image }</li> */}
+                <li className="categories-image"> <img src={ category.attributes.image } width="400" height="300"></img></li>
+                <li className="categories-text">{ category.attributes.name } </li>
              </Link>
             </div>
             )
