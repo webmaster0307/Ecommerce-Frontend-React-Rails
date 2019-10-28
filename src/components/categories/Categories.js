0a075db34027471c572939e6b878dfae41bb7f4a
webmaster0307 <web.master0307@gmail.com>
2019-10-28 11:11:32 -0400

    Refactor map function to outside of return

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -5,22 +5,22 @@ const Categories = (props) => {
     
     // console.log("categories props", props)
     // console.log("categoriesList.categories is", props.categoriesList.categories)
+    
+    let categories = props.categoriesList.categories ? props.categoriesList.categories.map(category => 
+        <div key={category.id}>
        
+        <Link to={`/category/${category.id}` }>
+            <br></br>
+            <h2 className="categories-text">{ category.attributes.name } </h2>
+            <li className="categories-image"> <img src={ category.attributes.image } ></img></li>
+         </Link>
+        </div>
+        )
+        : null
+    
     return (
         <div className="categories">
-            {props.categoriesList.categories ? props.categoriesList.categories.map(category => 
-            <div key={category.id}>
-           
-            <Link to={`/category/${category.id}` }>
-                <br></br>
-                {/* <li className="categories-image">{ category.attributes.image }</li> */}
-                <h2 className="categories-text">{ category.attributes.name } </h2>
-                <li className="categories-image"> <img src={ category.attributes.image } ></img></li>
-             </Link>
-            </div>
-            )
-            : null
-        }
+            {categories}
        </div>
      )
 }
