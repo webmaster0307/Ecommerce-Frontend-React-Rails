0780b8eae3ff3adb78dfc61aafd2bd7bf32ef53d
webmaster0307 <web.master0307@gmail.com>
2019-10-28 10:32:41 -0400

    Work on top margin css

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Categories = (props) => {
     
-    console.log("categories props", props)
+    // console.log("categories props", props)
     // console.log("categoriesList.categories is", props.categoriesList.categories)
        
     return (
