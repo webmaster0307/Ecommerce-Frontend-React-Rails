aad7046e0bad1859580df13298cab7a9f5870fa2
webmaster0307 <web.master0307@gmail.com>
2019-10-30 14:28:55 -0400

    Add color attributes to product

--- src/components/categories/Product.js
+++ src/components/categories/Product.js
@@ -7,7 +7,14 @@ const Product = (props) => {
     // console.log(props.match.params.id[0])
 
     let product = props.products.products.filter(product => product.id === props.match.params.id)[0]
-    // console.log("product is", product)
+    console.log("product is", product)
+
+    let productColors = product ? product.attributes.colors.map(color => 
+        <div key={color.id}>
+            <li className="product-text">{color.color_name}</li> 
+        </div>
+    ) : null
+        
 
     return(
         <div className="product-container">
@@ -18,11 +25,29 @@ const Product = (props) => {
             </div>
 
             <div className="col-6">
-                <li className="product-text bold">Style#: {product ? product.attributes.item_number : null}</li>
-                <li className="product-text bold">Name: {product ? product.attributes.name : null}</li>
+                <li className="product-text">Product Name: {product ? product.attributes.name : null}</li>
                 <li className="product-text">Description: {product ? product.attributes.description : null}</li>
+                <li className="product-text">Item Number: {product ? product.attributes.item_number : null}</li>
+
+                <br></br>
+                <li className="product-text">Colors: </li>
+                { productColors }
+                
+                <br></br>
                 <li className="product-text">Price: ${product ? product.attributes.price : null}</li>
-            </div>            
+
+                <br></br>
+
+                <li className="product-text">Select Qty: </li>
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+  
+                <button>Add to Cart</button>
+            </div>     
         </div>
     )
 }
