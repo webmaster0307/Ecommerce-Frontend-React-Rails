0b459379ad6ecdc467ed4310d6ceed9f60dec3cc
webmaster0307 <web.master0307@gmail.com>
2019-10-30 12:34:43 -0400

    Display product attributes

--- src/components/categories/Product.js
+++ src/components/categories/Product.js
@@ -4,10 +4,17 @@ import { connect } from 'react-redux'
 const Product = (props) => {
 
     console.log("product props", props);
+    console.log(props.match.params.id[0])
+
+    let product = props.products.products.filter(product => product.id === props.match.params.id)[0]
+    console.log("product is", product)
 
     return(
         <div>
-            product page
+            {product ? <img src={ product.attributes.image } alt="product" ></img> : null}
+            {product ? product.attributes.name : null}
+            {product ? product.attributes.description : null}
+            {product ? product.attributes.price : null}
         </div>
     )
 
