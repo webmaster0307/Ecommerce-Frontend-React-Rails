241c4268bd1f57609fa68b8f08276f9adfc0f75b
webmaster0307 <web.master0307@gmail.com>
2019-10-30 21:44:58 -0400

    Add categories stylesheet

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import './stylesheets/nav.css';
 import './stylesheets/form.css';
 import './stylesheets/products.css';
 import './stylesheets/product.css';
+import './stylesheets/categories.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import * as serviceWorker from './serviceWorker';
 
 
 
