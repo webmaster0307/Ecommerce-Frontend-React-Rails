c30d7178a38dc37a0bdf922358c32dda9ff3e363
webmaster0307 <web.master0307@gmail.com>
2019-11-04 22:33:32 -0500

    Add css styling to business home header

--- src/components/businesses/BusinessHome.js
+++ src/components/businesses/BusinessHome.js
@@ -4,8 +4,8 @@ import BusinessesContainer from '../../containers/BusinessesContainer';
 const BusinessHome = () => {
     return (
         <div className="business-home">
-            <h2>Current Businesses</h2>
-            <BusinessesContainer />
+            {/* <h2>Current Businesses</h2>
+            <BusinessesContainer /> */}
         </div>
     )
 
