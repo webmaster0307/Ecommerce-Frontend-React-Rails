22557d6101284fe00302a661db780cb974db42dc
webmaster0307 <web.master0307@gmail.com>
2019-11-08 10:59:45 -0500

    Add category and subcategory id

--- src/components/products/ProductNew.js
+++ src/components/products/ProductNew.js
@@ -51,19 +51,21 @@ class ProductNew extends Component {
 
                     <div className="row"> 
                     <div className="label">   
-                        <label htmlFor="name">Category:  </label>
+                        <label htmlFor="name">Category: Required  </label>
                             <select onChange={(e) => this.setState({selectedCategory: e.target.value})}>>
-                                {this.props.categories.categories.map((category) => <option key={category.attributes.id} id={category.attributes.id} value={category.id} >{category.attributes.name}</option>) }
+                            <option>Select an option.</option>
+                                {this.props.categories.categories.map((category) => <option key={category.id} id={category.id} value={category.id} >{category.attributes.name}</option>) }
                             </select>
                         </div>
                     </div>
 
                     <div className="row"> 
                     <div className="label">   
-                        <label htmlFor="name">SubCategory:  </label>
-                            <select onChange={(e) => this.setState({selectedSubcategory: e.target.value})}>>
-                                {this.props.subcategories.subcategories.map((subcategory) => <option key={subcategory.attributes.id} id={subcategory.attributes.id} value={subcategory.id} >{subcategory.attributes.name}</option>) }
-                            </select>
+                        <label htmlFor="name">SubCategory:  Required  </label>
+                            <select onChange={(e) => this.setState({selectedSubcategory: e.target.value})} >>
+                            <option>Select an option.</option>
+                                {this.props.subcategories.subcategories.map((subcategory) => <option key={subcategory.id} id={subcategory.id} value={subcategory.id} >{subcategory.attributes.name}</option>) }
+                            </select > 
                         </div>
                     </div>
 
@@ -94,7 +96,7 @@ class ProductNew extends Component {
                         <div className="label">  
                             <label htmlFor="image">Image:  </label>
                         </div>  
-                            <input type="text" name="image" id="image" placeholder="image" onChange={this.handleChange} />
+                            <input type="text" name="image" id="image" placeholder="image" onChange={this.handleChange} required/>
                     </div>
 
                     <div className="row">
