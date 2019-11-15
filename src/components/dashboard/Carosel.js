5eb5287d7c275778972e28cf4ab60af1616f87fa
webmaster0307 <web.master0307@gmail.com>
2019-11-15 10:22:41 -0500

    Complete onClick carosel

--- src/components/dashboard/Carosel.js
+++ src/components/dashboard/Carosel.js
@@ -8,46 +8,41 @@ class Carosel extends Component {
     super(props);
 
     this.state = { 
-        image1: "https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        image2: "https://images.pexels.com/photos/942317/pexels-photo-942317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        image3: "https://images.pexels.com/photos/1785138/pexels-photo-1785138.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        default: "https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         }
 }
 
-
     handleClickOne = (e) => {
         e.preventDefault();
-        // this.setState({
-        //     name: "",
-        //     tax_id: "",
-        // })
+        this.setState({
+            default: "https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        })
     }
 
     handleClickTwo = (e) => {
         e.preventDefault();
-        // this.setState({
-        //     name: "",
-        //     tax_id: "",
-        // })
+        this.setState({
+            default: "https://images.pexels.com/photos/942317/pexels-photo-942317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        })
     }
 
     handleClickThree = (e) => {
         e.preventDefault();
-        // this.setState({
-        //     name: "",
-        //     tax_id: "",
-        // })
+        this.setState({
+            default: "https://images.pexels.com/photos/1785138/pexels-photo-1785138.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        })
     }
+
     render() { 
         return (
             <div className="main">
                 <div className="carosel-image">
-                    <img className=""src={this.state.image1} alt="Photo" ></img>
+                    <img className=""src={this.state.default} alt="Photo" ></img>
                 </div>
                 <div className="carosel-dot">
-                    <FontAwesomeIcon className="dots" icon={faCircle} handleClick={this.handleClickOne} />
-                    <FontAwesomeIcon className="dots" icon={faCircle} handleClick={this.handleClickTwo} />
-                    <FontAwesomeIcon className="dots" icon={faCircle} handleClick={this.handleClickThree} />
+                    <FontAwesomeIcon className="dots" icon={faCircle} onClick={this.handleClickOne} />
+                    <FontAwesomeIcon className="dots" icon={faCircle} onClick={this.handleClickTwo} />
+                    <FontAwesomeIcon className="dots" icon={faCircle} onClick={this.handleClickThree} />
                 </div>  
             </div>
         )
