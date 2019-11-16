57bfbd1730335b58eb0b8d70eacfe16f1ffd004b
webmaster0307 <web.master0307@gmail.com>
2019-11-16 07:35:09 -0500

    Update carosel text

--- src/components/dashboard/ControlledCarousel.js
+++ src/components/dashboard/ControlledCarousel.js
@@ -13,20 +13,20 @@ const ControlledCarousel = () => {
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <h3>One Stop Shop!</h3>
+                {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block w-100"
                 src="https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                height="562" alt="Second slide"
+                height="485" alt="Second slide"
               />
 
               <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3>TThe latest trends in technology.</h3>
+                {/* <p>The latest trends.</p> */}
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -36,8 +36,8 @@ const ControlledCarousel = () => {
                 alt="Third slide"
               />
               <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                <h3 >Best prices all the time</h3>
+                {/* <p className="image3-color" >Best prices all the time.</p> */}
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
